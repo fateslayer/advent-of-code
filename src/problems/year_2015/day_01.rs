4943fcb5b8e2d77be685385be10f0e87db91cdb4
Ajaz Ur Rehman <fateslayer47@gmail.com>
2025-09-18 23:58:51 +0530

    Add solution for first problem

--- src/problems/year_2015/day_01.rs
+++ src/problems/year_2015/day_01.rs
@@ -1,3 +1,23 @@
+// Problem: https://adventofcode.com/2015/day/1
+
+use std::fs;
+
 pub fn solve() {
-    println!("Day 1");
+    let input = fs::read_to_string("inputs/2015/01.txt").unwrap();
+
+    let mut count = 0;
+
+    for (i, c) in input.chars().enumerate() {
+        match c {
+            '(' => count += 1,
+            ')' => count -= 1,
+            _ => panic!("Unsupported char"),
+        }
+
+        if count == -1 {
+            println!("Basement: {}", i + 1);
+        }
+    }
+
+    println!("Count: {}", count);
 }
