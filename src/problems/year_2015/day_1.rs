2324fe0f9d85c3e5bb990069d438af3ba581fcaf
Ajaz Ur Rehman <fateslayer47@gmail.com>
2025-09-23 10:31:38 +0530

    Refactor code

--- src/problems/year_2015/day_1.rs
+++ src/problems/year_2015/day_1.rs
@@ -1,6 +1,28 @@
 // Problem: https://adventofcode.com/2015/day/1
 
 pub fn solve(input: &str) {
+    let part1 = solve_part1(input);
+    let part2 = solve_part2(input);
+
+    println!("Part 1: {}", part1);
+    println!("Part 2: {}", part2.unwrap());
+}
+
+fn solve_part1(input: &str) -> i32 {
+    let mut count = 0;
+
+    for c in input.chars() {
+        match c {
+            '(' => count += 1,
+            ')' => count -= 1,
+            _ => continue,
+        }
+    }
+
+    count
+}
+
+fn solve_part2(input: &str) -> Option<usize> {
     let mut count = 0;
     let mut basement: Option<usize> = None;
 
@@ -13,9 +35,9 @@ pub fn solve(input: &str) {
 
         if count == -1 && basement.is_none() {
             basement = Some(i + 1);
+            break;
         }
     }
 
-    println!("Part 1: {}", count);
-    println!("Part 2: {}", basement.unwrap());
+    basement
 }
