89f0f4c1b6d91a57040bc353f14f8619833a5a01
Ajaz Ur Rehman <fateslayer47@gmail.com>
2025-09-25 11:17:29 +0530

    Refactor code

--- src/problems/year_2015/day_4/part1.rs
+++ src/problems/year_2015/day_4/part1.rs
@@ -2,11 +2,12 @@ use md5;
 
 pub fn solve(input: &str) -> i32 {
     let mut num = 1;
+    let pattern = "00000";
 
     loop {
         let digest = md5::compute(format!("{}{}", input, num));
 
-        if format!("{:x}", digest).starts_with("00000") {
+        if format!("{:x}", digest).starts_with(pattern) {
             break;
         }
 
--- src/problems/year_2015/day_4/part2.rs
+++ src/problems/year_2015/day_4/part2.rs
@@ -2,11 +2,12 @@ use md5;
 
 pub fn solve(input: &str) -> i32 {
     let mut num = 1;
+    let pattern = "000000";
 
     loop {
         let digest = md5::compute(format!("{}{}", input, num));
 
-        if format!("{:x}", digest).starts_with("000000") {
+        if format!("{:x}", digest).starts_with(pattern) {
             break;
         }
 
