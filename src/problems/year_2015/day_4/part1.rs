2bb0dcf0d8cbdabe401d54f50889187b18ce5af8
Ajaz Ur Rehman <fateslayer47@gmail.com>
2025-09-25 11:16:21 +0530

    Solve problem 4

--- src/problems/year_2015/day_4/part1.rs
+++ src/problems/year_2015/day_4/part1.rs
@@ -1,3 +1,17 @@
+use md5;
+
 pub fn solve(input: &str) -> i32 {
-    0
+    let mut num = 1;
+
+    loop {
+        let digest = md5::compute(format!("{}{}", input, num));
+
+        if format!("{:x}", digest).starts_with("00000") {
+            break;
+        }
+
+        num += 1;
+    }
+
+    num
 }
