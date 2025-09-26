d470c491903a9c81d6a4954499e8da543b257a8b
Ajaz Ur Rehman <fateslayer47@gmail.com>
2025-09-26 13:59:45 +0530

    Add problem 5

---
+++ src/problems/year_2015/day_5/mod.rs
@@ -0,0 +1,10 @@
+mod part1;
+mod part2;
+
+pub fn solve(input: &str) {
+    let part1_result = part1::solve(input);
+    // let part2_result = part2::solve(input);
+
+    println!("Part 1: {}", part1_result);
+    // println!("Part 2: {}", part2_result);
+}
