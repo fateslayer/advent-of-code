572d83dbfe5a3a28f043c3bf8afdbb4025c871c3
Ajaz Ur Rehman <fateslayer47@gmail.com>
2025-09-27 12:47:52 +0530

    Solve part 2

--- src/problems/year_2015/day_5/part2.rs
+++ src/problems/year_2015/day_5/part2.rs
@@ -1,29 +1,12 @@
+use fancy_regex::Regex;
+
 pub fn solve(input: &str) -> i32 {
     let mut count = 0;
+    let consecutive = Regex::new(r"([a-zA-Z]{2}).*?\1").unwrap();
+    let repeated = Regex::new(r"([a-zA-Z]).\1").unwrap();
 
     for line in input.lines() {
-        let mut vowels = 0;
-        let mut duplicate = false;
-        let mut invalid = false;
-
-        for (i, c) in line.chars().enumerate() {
-            if "aeiou".contains(c) {
-                vowels += 1;
-            }
-
-            if !duplicate && i > 0 && line.chars().nth(i - 1).unwrap() == c {
-                duplicate = true;
-            }
-
-            if i > 0 {
-                match &line[i - 1..=i] {
-                    "ab" | "cd" | "pq" | "xy" => invalid = true,
-                    _ => (),
-                }
-            }
-        }
-
-        if vowels >= 3 && duplicate && !invalid {
+        if consecutive.is_match(line).unwrap() && repeated.is_match(line).unwrap() {
             count += 1;
         }
     }
