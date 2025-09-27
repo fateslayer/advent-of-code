0da8a4881f347f50ffa9af270ce7c68a25d061fe
Ajaz Ur Rehman <fateslayer47@gmail.com>
2025-09-27 14:02:29 +0530

    Solve problem 6

--- src/problems/year_2015/day_6/part1.rs
+++ src/problems/year_2015/day_6/part1.rs
@@ -1,3 +1,40 @@
+use fancy_regex::Regex;
+
 pub fn solve(input: &str) -> i32 {
-    0
+    const SIZE: usize = 1000;
+    let mut grid = [[false; SIZE]; SIZE];
+    let re = Regex::new(r"(turn on|turn off|toggle) (\d+),(\d+) through (\d+),(\d+)").unwrap();
+
+    for line in input.lines() {
+        if let Ok(Some(captures)) = re.captures(line) {
+            let action = &captures[1];
+            let x1: usize = captures[2].parse().unwrap();
+            let y1: usize = captures[3].parse().unwrap();
+            let x2: usize = captures[4].parse().unwrap();
+            let y2: usize = captures[5].parse().unwrap();
+
+            for i in x1..=x2 {
+                for j in y1..=y2 {
+                    match action {
+                        "turn on" => grid[i][j] = true,
+                        "turn off" => grid[i][j] = false,
+                        "toggle" => grid[i][j] = !grid[i][j],
+                        _ => panic!("unknown"),
+                    }
+                }
+            }
+        }
+    }
+
+    let mut turned_on: i32 = 0;
+
+    for row in grid {
+        for item in row {
+            if item {
+                turned_on += 1;
+            }
+        }
+    }
+
+    turned_on
 }
