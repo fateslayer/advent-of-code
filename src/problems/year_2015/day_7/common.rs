f48e8cfd961c0a93b852e03b5453cdb3a28f11a1
Ajaz Ur Rehman <fateslayer47@gmail.com>
2025-10-11 20:52:13 +0530

    Refactor code for day 7

--- src/problems/year_2015/day_7/common.rs
+++ src/problems/year_2015/day_7/common.rs
@@ -1,6 +1,17 @@
 use std::collections::HashMap;
 
-use fancy_regex::Regex;
+use regex::Regex;
+
+use once_cell::sync::Lazy;
+
+static INPUT_RE: Lazy<Regex> = Lazy::new(|| Regex::new(r"(.+) -> ([a-z]+)").unwrap());
+static NUM_RE: Lazy<Regex> = Lazy::new(|| Regex::new(r"^\d+$").unwrap());
+static WIRE_RE: Lazy<Regex> = Lazy::new(|| Regex::new(r"^[a-z]+$").unwrap());
+static NOT_RE: Lazy<Regex> = Lazy::new(|| Regex::new(r"^NOT ([a-z]+)$").unwrap());
+static OR_RE: Lazy<Regex> = Lazy::new(|| Regex::new(r"^(.+) OR (.+)$").unwrap());
+static AND_RE: Lazy<Regex> = Lazy::new(|| Regex::new(r"^(.+) AND (.+)$").unwrap());
+static LSHIFT_RE: Lazy<Regex> = Lazy::new(|| Regex::new(r"^(.+) LSHIFT (.+)$").unwrap());
+static RSHIFT_RE: Lazy<Regex> = Lazy::new(|| Regex::new(r"^(.+) RSHIFT (.+)$").unwrap());
 
 pub enum Instruction {
     Value(u32),
@@ -13,40 +24,32 @@ pub enum Instruction {
 }
 
 fn parse_instruction(input: &str) -> Instruction {
-    let num_re = Regex::new(r"^\d+$").unwrap();
-    let string_re = Regex::new(r"^[a-z]+$").unwrap();
-    let not_re = Regex::new(r"^NOT ([a-z]+)$").unwrap();
-    let or_re = Regex::new(r"^(.+) OR (.+)$").unwrap();
-    let and_re = Regex::new(r"^(.+) AND (.+)$").unwrap();
-    let lshift_re = Regex::new(r"^(.+) LSHIFT (.+)$").unwrap();
-    let rshift_re = Regex::new(r"^(.+) RSHIFT (.+)$").unwrap();
-
-    if let Ok(Some(captures)) = num_re.captures(input) {
+    if let Some(captures) = NUM_RE.captures(input) {
         let value = &captures[0];
         let value = value.parse().unwrap();
         Instruction::Value(value)
-    } else if let Ok(Some(captures)) = string_re.captures(input) {
+    } else if let Some(captures) = WIRE_RE.captures(input) {
         let value = captures[0].to_string();
         Instruction::Wire(value)
-    } else if let Ok(Some(captures)) = not_re.captures(input) {
+    } else if let Some(captures) = NOT_RE.captures(input) {
         let value = captures[1].to_string();
         Instruction::Not(value)
-    } else if let Ok(Some(captures)) = and_re.captures(input) {
+    } else if let Some(captures) = AND_RE.captures(input) {
         let left = captures[1].to_string();
         let right = captures[2].to_string();
 
         Instruction::And(left, right)
-    } else if let Ok(Some(captures)) = or_re.captures(input) {
+    } else if let Some(captures) = OR_RE.captures(input) {
         let left = captures[1].to_string();
         let right = captures[2].to_string();
 
         Instruction::Or(left, right)
-    } else if let Ok(Some(captures)) = lshift_re.captures(input) {
+    } else if let Some(captures) = LSHIFT_RE.captures(input) {
         let left = captures[1].to_string();
         let right = captures[2].to_string();
 
         Instruction::LShift(left, right)
-    } else if let Ok(Some(captures)) = rshift_re.captures(input) {
+    } else if let Some(captures) = RSHIFT_RE.captures(input) {
         let left = captures[1].to_string();
         let right = captures[2].to_string();
 
@@ -57,12 +60,10 @@ fn parse_instruction(input: &str) -> Instruction {
 }
 
 pub fn get_instruction_map(input: &str) -> HashMap<String, Instruction> {
-    let input_re = Regex::new(r"(.+) -> ([a-z]+)").unwrap();
-
     let mut map = HashMap::new();
 
     for line in input.lines() {
-        if let Ok(Some(captures)) = input_re.captures(line) {
+        if let Some(captures) = INPUT_RE.captures(line) {
             let input = captures[1].to_string();
             let wire = captures[2].to_string();
             let instruction = parse_instruction(&input);
