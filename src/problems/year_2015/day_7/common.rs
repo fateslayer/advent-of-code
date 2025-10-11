cb9db5e834b3bdaa972c58bc3ab7496acace9f2b
Ajaz Ur Rehman <fateslayer47@gmail.com>
2025-10-11 20:59:13 +0530

    Refactor code

--- src/problems/year_2015/day_7/common.rs
+++ src/problems/year_2015/day_7/common.rs
@@ -74,3 +74,41 @@ pub fn get_instruction_map(input: &str) -> HashMap<String, Instruction> {
 
     map
 }
+
+pub fn find_value(
+    signal: &str,
+    map: &HashMap<String, Instruction>,
+    cache: &mut HashMap<String, u32>,
+) -> u32 {
+    if let Some(&cached) = cache.get(signal) {
+        return cached;
+    }
+
+    if let Ok(num) = signal.parse::<u32>() {
+        return num;
+    }
+
+    let input = map.get(signal).unwrap();
+
+    let result: u32 = match input {
+        Instruction::Value(value) => *value,
+        Instruction::Wire(value) => find_value(&value, map, cache),
+        Instruction::Not(value) => (!find_value(&value, map, cache)) & 0xFFFF,
+        Instruction::And(left, right) => {
+            find_value(&left, map, cache) & find_value(&right, map, cache)
+        }
+        Instruction::Or(left, right) => {
+            find_value(&left, map, cache) | find_value(&right, map, cache)
+        }
+        Instruction::LShift(left, right) => {
+            find_value(&left, map, cache) << find_value(&right, map, cache)
+        }
+        Instruction::RShift(left, right) => {
+            find_value(&left, map, cache) >> find_value(&right, map, cache)
+        }
+    };
+
+    cache.insert(signal.to_string(), result);
+
+    result
+}
