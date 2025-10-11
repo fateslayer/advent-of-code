6499463000f5e0628f8be17962cde75aaa36a4cb
Ajaz Ur Rehman <fateslayer47@gmail.com>
2025-10-11 21:12:42 +0530

    Solve part 2

--- src/problems/year_2015/day_7/part2.rs
+++ src/problems/year_2015/day_7/part2.rs
@@ -1,3 +1,14 @@
+use super::common::{Instruction, find_value, get_instruction_map};
+use super::part1;
+use std::collections::HashMap;
+
 pub fn solve(input: &str) -> u32 {
-    0
+    let previous_value = part1::solve(input);
+    let mut map = get_instruction_map(input);
+
+    map.insert("b".to_string(), Instruction::Value(previous_value));
+
+    let mut cache: HashMap<String, u32> = HashMap::new();
+
+    find_value("a", &map, &mut cache)
 }
